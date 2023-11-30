adc55e80696edaa4ca774437932eab245fcc1739
hygl <p.huegelmeyer@gmail.com>
2023-11-30 11:59:59 +0100

    Fixed Blog Post Rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://hygl.github.io',
   integrations: [sitemap(), tailwind()]
 });
