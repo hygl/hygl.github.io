4a71a2b4cd8ec05f7b6fae0378d0012b24d98d23
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2023-12-04 10:38:26 +0100

    removed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hygl.github.io',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap()]
 });
