b974349349ecb7e20a3e3048c53fc606f09dc32f
Philipp Hügeömeyer <p.huegelmeyer@gmail.com>
2024-12-26 07:54:23 +0100

    Migrate to astro 5.1

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+
+// 4. Export a single `collections` object to register your collection(s)
 const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string().optional(),
