f0f56d365ac458c3503f67247d6afc21da12bf4b
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2025-03-17 08:02:11 +0100

    astro 5.5, anderes Ziel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hygl.github.io',
-  integrations: [sitemap()],
+  site: 'https://hygl.codeberg.page',
+  integrations: [sitemap(), compressor()],
   redirects: {
     '/': '/1'
   }
